2af2738823b15b9fc2fb5b753fbbc840a49a9f1d
Sergei Belialov <sbelialov@rrc.ca>
2026-01-22 17:20:54 -0600

    test data

--- src/service/playerService.ts
+++ src/service/playerService.ts
@@ -5,3 +5,27 @@ interface Player {
     losses: number,
     totalScore: number
 }
+
+let playerData: Player[] = [
+    {
+        id: 1,
+        name: "ShadowStrike",
+        wins: 15,
+        losses: 5,
+        totalScore: 2850
+    },
+    {
+        id: 2,
+        name: "NoobMaster",
+        wins: 3,
+        losses: 12,
+        totalScore: 4200
+    }
+    , {
+        id: 3,
+        name: "ProGamer99",
+        wins: 0,
+        losses: 0,
+        totalScore: 0
+    }
+];
