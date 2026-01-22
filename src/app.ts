acf8b1692593643b3940ee6e450cf880d961d584
Sergei Belialov <sbelialov@rrc.ca>
2026-01-22 17:09:58 -0600

    init comit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express,  { Express } from "express";
+
+let app: Express =  express();
+
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    });
+});
+
+export default app;
