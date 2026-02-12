ab513c0d76f2bd2cbfb3ee776162bf996759fef1
Sergei Belialov <sbelialov@rrc.ca>
2026-02-12 17:58:30 -0600

    middleware

--- src/v1/routes/itemsRoutes.ts
+++ src/v1/routes/itemsRoutes.ts
@@ -7,11 +7,14 @@ import {
     updateItem
 } from "../controllers/itemsController";
 
+import { validateRequest } from "../middleware/validate";
+import { itemSchemas } from "../validation/itemsSchemas";
+
 const itemsRoutes: Router = express.Router();
 
 itemsRoutes.get("/", getAllItem);
 itemsRoutes.get("/:id", getItemById )
-itemsRoutes.post("/", createItem);
+itemsRoutes.post("/",  validateRequest(itemSchemas.create), createItem);
 itemsRoutes.put("/:id", updateItem);
 itemsRoutes.delete("/:id", deleteItem);
 
